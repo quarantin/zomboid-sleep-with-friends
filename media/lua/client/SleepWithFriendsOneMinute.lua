b8067418568a0e268f18fa747ce5c94585a8c318
Corentin Delorme <codelorme@gmail.com>
2021-12-22 22:28:48 +0100

    Add sandbox settings to choose how long game characters have to sleep on average

--- media/lua/client/SleepWithFriendsOneMinute.lua
+++ media/lua/client/SleepWithFriendsOneMinute.lua
@@ -2,7 +2,8 @@ function mpSleep()
 	local player = getPlayer()
 	if player:isAsleep() then
 		local currentFatigue = player:getStats():getFatigue()
-		player:getStats():setFatigue(math.max(0, currentFatigue - 0.008))
+		local subtractFatigue = 0.016 / SandboxVars.SleepWithFriends.SleepLength
+		player:getStats():setFatigue(math.max(0, currentFatigue - subtractFatigue))
 		if player:getStats():getFatigue() <= 0 then
 			player:forceAwake()
 		end
