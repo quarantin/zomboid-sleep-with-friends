2ba6334abae5cb4cf8fefdd21669c715ba8c3de7
KS-Snuggles <96385981+KS-Snuggles@users.noreply.github.com>
2021-12-24 20:56:28 -0500

    Update SleepWithFriendsOneMinute.lua
    Added endurance recovery, set to double as fast as fatigue recovery

--- media/lua/client/SleepWithFriendsOneMinute.lua
+++ media/lua/client/SleepWithFriendsOneMinute.lua
@@ -2,8 +2,11 @@ function mpSleep()
 	local player = getPlayer()
 	if player:isAsleep() then
 		local currentFatigue = player:getStats():getFatigue()
+		local currentEndurance = player:getStats():getEndurance()
 		local subtractFatigue = 0.016 / SandboxVars.SleepWithFriends.SleepLength
+		local addEndurance = 0.032 / SandboxVars.SleepWithFriends.SleepLength
 		player:getStats():setFatigue(math.max(0, currentFatigue - subtractFatigue))
+		player:getStats():setEndurance(math.max(0, currentEndurance + addEndurance))
 		if player:getStats():getFatigue() <= 0 then
 			player:forceAwake()
 		end
