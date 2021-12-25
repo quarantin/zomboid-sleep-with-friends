8f74f385c123c90c1295bba434ce7a878d49da4b
Snuggles <96385981+KS-Snuggles@users.noreply.github.com>
2021-12-25 03:30:08 -0500

    Update SleepWithFriendsOneMinute.lua
    local serverDayLength = (SandboxVars.DayLength - 2) added as proof of concept pulling server daytime setting. the -2 is to simplify math, as 1 hour is option 3, 2 hours option 4, and so on. Basically, forget 30 and 15 min servers lmao
    
    added local AutoWake = SandboxVars.SleepWithFriends.AutoWake to set as new variable. Likely redundant and can be removed if found so.
    
    added AutoWake == 1 to enable toggle

--- media/lua/client/SleepWithFriendsOneMinute.lua
+++ media/lua/client/SleepWithFriendsOneMinute.lua
@@ -1,13 +1,15 @@
 function mpSleep()
 	local player = getPlayer()
 	if player:isAsleep() then
+		local serverDayLength = (SandboxVars.DayLength - 2)
+		local AutoWake = SandboxVars.SleepWithFriends.AutoWake
 		local currentFatigue = player:getStats():getFatigue()
 		local currentEndurance = player:getStats():getEndurance()
 		local subtractFatigue = 0.016 / SandboxVars.SleepWithFriends.SleepLength
 		local addEndurance = 0.024 / SandboxVars.SleepWithFriends.SleepLength
 		player:getStats():setFatigue(math.max(0, currentFatigue - subtractFatigue))
 		player:getStats():setEndurance(math.max(0, currentEndurance + addEndurance))
-		if player:getStats():getFatigue() <= 0 then
+		if player:getStats():getFatigue() <= 0 and AutoWake == 1 then
 			player:forceAwake()
 		end
 	end
